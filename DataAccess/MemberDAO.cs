05c5815b594fd6780dd4785f8a35a2853b0fbcef
Minh Quang <quangpro107@gmail.com>
2023-12-05 14:07:04 +0700

    Group project

---
+++ DataAccess/MemberDAO.cs
@@ -0,0 +1,250 @@
+﻿using System;
+using BusinessObject;
+using Microsoft.Extensions.Configuration;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace DataAccess
+{
+    public class MemberDAO
+    {
+        // Get default user from appsettings
+        private MemberObject GetDefaultMember()
+        {
+            MemberObject Default = null;
+            using (StreamReader r = new StreamReader("appsettings.json"))
+            {
+                string json = r.ReadToEnd();
+                IConfiguration config = new ConfigurationBuilder()
+                                        .SetBasePath(Directory.GetCurrentDirectory())
+                                        .AddJsonFile("appsettings.json", true, true)
+                                        .Build();
+                string email = config["account:defaultAccount:email"];
+                string password = config["account:defaultAccount:password"];
+                Default = new MemberObject
+                {
+                    MemberID = 1,
+                    Email = email,
+                    Password = password,
+                    City = "",
+                    Country = "",
+                    MemberName = "Admin"
+                };
+            }
+
+            return Default;
+        }
+
+        // Initialize MemberList
+        private static List<MemberObject> members = new List<MemberObject>
+        {
+            new MemberObject
+            {
+                MemberID = 2,
+                MemberName = "Nguyen Van Son",
+                Email = "son@gmail.com",
+                Password = "123",
+                City = "California",
+                Country = "United States"
+            },
+            new MemberObject
+            {
+                MemberID = 3,
+                MemberName = "Jackie Nguyen",
+                Email = "jackie@gmail.com",
+                Password = "1234",
+                City = "Sha Tin",
+                Country = "Hong Kong"
+            },
+            new MemberObject
+            {
+                MemberID = 4,
+                MemberName = "Bill Smitt",
+                Email = "bsmitt@gmail.com",
+                Password = "abc",
+                City = "Manchester United",
+                Country = "British"
+            },
+            new MemberObject
+            {
+                MemberID = 5,
+                MemberName = "Micheal Tom",
+                Email = "mtom@gmail.com",
+                Password = "123",
+                City = "Zürich",
+                Country = "Switzerland"
+            },
+            new MemberObject
+            {
+                MemberID = 6,
+                MemberName = "Jack Dawson",
+                Email = "daw@gmail.com",
+                Password = "jack4",
+                City = "Opera",
+                Country = "Australia"
+            },
+            new MemberObject
+            {
+                MemberID = 7,
+                MemberName = "Lerbon James",
+                Email = "ler@gmail.com",
+                Password = "ler",
+                City = "Chicago",
+                Country = "United States"
+            }
+        };
+        private MemberDAO()
+        {
+            MemberObject DefaultAdmin = GetDefaultMember();
+            if (DefaultAdmin != null)
+            {
+                members.Add(DefaultAdmin);
+            }
+        }
+
+        // Using Singleton Pattern
+        private static MemberDAO instance = null;
+        private static object instanceLook = new object();
+
+        public static MemberDAO Instance
+        {
+            get
+            {
+                lock (instanceLook)
+                {
+                    if (instance == null)
+                    {
+                        instance = new MemberDAO();
+                    }
+                    return instance;
+                }
+            }
+        }
+
+        public List<MemberObject> GetMembersList => members;
+
+        public MemberObject Login(string Email, string Password)
+        {
+            MemberObject member = members.SingleOrDefault(mb => mb.Email.Equals(Email) && mb.Password.Equals(Password));
+            return member;
+        }
+
+        public MemberObject GetMember(int MemberId)
+        {
+            return members.SingleOrDefault(mb => mb.MemberID == MemberId);
+        }
+        public MemberObject GetMember(string MemberEmail)
+        {
+            return members.SingleOrDefault(mb => mb.Email.Equals(MemberEmail));
+        }
+        public void AddMember(MemberObject member)
+        {
+            if (member == null)
+            {
+                throw new Exception("Member is undefined!");
+            }
+
+            if (GetMember(member.MemberID) == null && GetMember(member.Email) == null)
+            {
+                members.Add(member);
+            }
+            else
+            {
+                throw new Exception("Member is existed!");
+            }
+        }
+        public void Update(MemberObject member)
+        {
+            if (member == null)
+            {
+                throw new Exception("Member is undefined!");
+            }
+            MemberObject mem = GetMember(member.MemberID);
+            if (mem != null)
+            {
+                var index = members.IndexOf(mem);
+                members[index] = member;
+            }
+            else
+            {
+                throw new Exception("Member does not exist!");
+            }
+        }
+        public void Delete(int MemberId)
+        {
+            MemberObject member = GetMember(MemberId);
+            if (member != null)
+            {
+                members.Remove(member);
+            }
+            else
+            {
+                throw new Exception("Member does not exist!");
+            }
+        }
+
+        public IEnumerable<MemberObject> SearchMember(int id)
+        {
+            IEnumerable<MemberObject> searchResult = null;
+
+            var memberSearch = from member in members
+                               where member.MemberID == id
+                               select member;
+            searchResult = memberSearch;
+
+            return searchResult;
+        }
+        public IEnumerable<MemberObject> SearchMember(string name)
+        {
+            IEnumerable<MemberObject> searchResult = null;
+
+            var memberSearch = from member in members
+                               where member.MemberName.ToLower().Contains(name.ToLower())
+                               select member;
+            searchResult = memberSearch;
+
+            return searchResult;
+        }
+
+        public IEnumerable<MemberObject> FilterMemberByCountry(string country, IEnumerable<MemberObject> searchList)
+        {
+            IEnumerable<MemberObject> searchResult = null;
+
+            var memberSearch = from member in searchList
+                               where member.Country == country
+                               select member;
+            if (country.Equals("All"))
+            {
+                memberSearch = from member in searchList
+                               select member;
+            }
+            searchResult = memberSearch;
+
+            return searchResult;
+        }
+
+        public IEnumerable<MemberObject> FilterMemberByCity(string country, string city, IEnumerable<MemberObject> searchList)
+        {
+            IEnumerable<MemberObject> searchResult = null;
+
+            var memberSearch = from member in searchList
+                               where member.City == city
+                               select member;
+            if (city.Equals("All"))
+            {
+                memberSearch = from member in searchList
+                               where member.Country == country
+                               select member;
+                if (country.Equals("All"))
+                {
+                    memberSearch = from member in searchList
+                                   select member;
+                }
+            }
+            searchResult = memberSearch;
+
+            return searchResult;
+        }
+    }
+}
